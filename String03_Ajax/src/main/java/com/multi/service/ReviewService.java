59dab900144fafbf4789872960fa51f02178fa64
Ajin <aaajin1220@gmail.com>
2024-03-25 21:05:45 +0900

    240325 수업(spring MVC 웹-ajax) 상품리뷰만들기

---
+++ String03_Ajax/src/main/java/com/multi/service/ReviewService.java
@@ -0,0 +1,20 @@
+package com.multi.service;
+
+import java.util.List;
+
+import com.multi.domain.ReviewVO;
+
+public interface ReviewService {
+
+	int insertReview(ReviewVO rvo);
+	
+	List<ReviewVO> getReviewList(int pnum); //특정 상품에 대한 리뷰 글 목록
+	
+	int getReviewCount(int pnum);
+	
+	ReviewVO getReview(int no);//리뷰글번호(PK)로 상품글 가져오기
+	
+	int deleteReview(int no);
+	
+	int updateReview(ReviewVO rvo);
+}
